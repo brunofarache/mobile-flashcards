ea449194532cc246fb1452887f47e7c0550addd5
Bruno Farache <bruno.farache@liferay.com>
2019-06-10 23:08:40 -0300

    add deck

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
 
@@ -12,31 +12,50 @@ function Decks() {
 }
 
 function AddDeck() {
+	const [deckName, setDeckName] = useState('');
+
+	const onPressCreateDeck = () => {
+		alert(deckName);
+	};
+
 	return (
 		<View style={styles.container}>
-			<Text>Add Deck</Text>
+			<Text>What's the title of your new deck?</Text>
+			<TextInput
+				style={{height: 40, width: 300, borderColor: 'gray', borderWidth: 1}}
+				onChangeText={(text) => setDeckName(text)}
+			/>
+			<Button
+				title="Create Deck"
+				onPress={onPressCreateDeck}
+			/>
 		</View>
 	);
 }
 
 export default function App() {
-	const TabNavigator = createBottomTabNavigator({
-		Decks: {
-			screen: Decks,
-			navigationOptions: {
-				tabBarIcon: ({ tintColor }) =>
-					<MaterialCommunityIcons name="cards" size={30} color={tintColor} />
+	const TabNavigator = createBottomTabNavigator(
+		{
+			Decks: {
+				screen: Decks,
+				navigationOptions: {
+					tabBarIcon: ({ tintColor }) =>
+						<MaterialCommunityIcons name="cards" size={30} color={tintColor} />
+				},
 			},
+			AddDeck: {
+				screen: AddDeck,
+				navigationOptions: {
+					tabBarLabel: 'Add Deck',
+					tabBarIcon: ({ tintColor }) =>
+						<FontAwesome name="plus-square" size={30} color={tintColor} />
+				},
+			}
 		},
-		AddDeck: {
-			screen: AddDeck,
-			navigationOptions: {
-				tabBarLabel: 'Add Deck',
-				tabBarIcon: ({ tintColor }) =>
-					<FontAwesome name="plus-square" size={30} color={tintColor} />
-			},
+		{
+			initialRouteName: 'AddDeck'
 		}
-	});
+	);
 
 	const AppContainer = createAppContainer(TabNavigator);
 
