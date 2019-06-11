2ba343349145730040b32182f91f0741663ef555
Bruno Farache <bruno.farache@liferay.com>
2019-06-10 21:47:56 -0300

    add tabbar

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+
+function Decks() {
+	return (
+		<View style={styles.container}>
+			<Text>Decks</Text>
+		</View>
+	);
+}
+
+function AddDeck() {
+	return (
+		<View style={styles.container}>
+			<Text>Add Deck</Text>
+		</View>
+	);
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const TabNavigator = createBottomTabNavigator({
+		Home: Decks,
+		AddDeck: AddDeck
+	});
+
+	const AppContainer = createAppContainer(TabNavigator);
+
+	return (
+		<AppContainer />
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
